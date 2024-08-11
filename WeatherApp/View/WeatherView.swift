4715adb754e61f0c9b7ce4f16a7443edb81bb696
Dren <drenuruqi95@gmail.com>
2024-08-11 15:50:53 +0200

    Created the Alerts and displayed them correctly, also fixed the FrontEnd part of the App and displayed the image from the Response

--- WeatherApp/View/WeatherView.swift
+++ WeatherApp/View/WeatherView.swift
@@ -9,60 +9,89 @@ import SwiftUI
 import SDWebImageSwiftUI
 
 struct WeatherView: View {
-    @StateObject var viewModal = WeatherViewModel()
+    @StateObject var viewModel = WeatherViewModel()
     @State private var searchBtn: Bool = false
     @State private var cityTxtLabel: String = ""
-    @State private var defaultSymbol = "https://www.google.com/search?q=weather+symbol&sca_esv=67842c87415e33f5&sca_upv=1&udm=2&biw=1440&bih=710&ei=Ldi3ZoLIBcKTxc8PuK-yiQU&ved=0ahUKEwjCktjyq-uHAxXCSfEDHbiXLFEQ4dUDCBA&uact=5&oq=weather+symbol&gs_lp=Egxnd3Mtd2l6LXNlcnAiDndlYXRoZXIgc3ltYm9sMggQABiABBixAzIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABEjpG1DPBFjXGHADeACQAQCYAW-gAb4KqgEDNy43uAEDyAEA-AEBmAIRoAL5CsICChAAGIAEGEMYigXCAgsQABiABBixAxiDAcICDhAAGIAEGLEDGIMBGIoFmAMAiAYBkgcDOS44oAeYRg&sclient=gws-wiz-serp#vhid=h0BKTsfhNu49AM&vssid=mosaic"
     
     var body: some View {
         VStack {
             HStack(alignment: .center) {
                 TextField("City Name", text: $cityTxtLabel)
-                    .frame(width: 280, height: 30, alignment: .center)
+                    .frame(width: 250, height: 30, alignment: .center)
                     .border(Color.black, width: 2)
-                Button("Search") {
+                Button("Get Weather") {
                     searchBtn.toggle()
                 }
-                .frame(width: 70, height: 30)
+                .frame(width: 110, height: 30)
                 .foregroundColor(Color.black)
                 .background(Color.blue.opacity(0.7))
             } .padding(.vertical)
-            HStack {
-                Text(viewModal.weatherData?.location.name ?? "City")
+            HStack(spacing: 60) {
+                Text(viewModel.weatherData?.location.name ?? "City")
                     .frame(width: 100, height: 35)
                     .foregroundColor(.black)
-                Text(viewModal.weatherData?.location.country ?? "Country")
+                    .font(.bold(.headline)())
+                Text(viewModel.weatherData?.location.country ?? "Country")
                     .frame(width: 100, height: 35)
                     .foregroundColor(.black)
-                Text(viewModal.weatherData?.location.localtime ?? "TimeLine")
-                    .frame(width: 100, height: 35)
-                    .foregroundColor(.black)
+                    .font(.bold(.headline)())
             } .padding(.vertical, 20)
-            HStack {
-                Text("\(viewModal.weatherData?.current.tempC ?? 0.0)")
-                    .frame(width: 100, height: 35)
+            HStack(alignment: .center, spacing: 20) {
+                Text("""
+                    Local Time:
+                    \(viewModel.weatherData?.location.localtime ?? "No data")
+                    """)
+                    .frame(width: 150)
                     .foregroundColor(.black)
-                Text("\(viewModal.weatherData?.current.windKph ?? 0.0)")
-                    .frame(width: 100, height: 35)
+                    .font(.bold(.headline)())
+                Text("""
+                    Last time updated:
+                    \(viewModel.weatherData?.current.lastUpdated ?? "No data")
+                    """)
+                    .frame(width: 150)
                     .foregroundColor(.black)
-                Text(viewModal.weatherData?.current.condition.text ?? "Condition")
-                    .frame(width: 100, height: 35)
-                    .foregroundColor(.black)
+                    .font(.bold(.headline)())
             } .padding(.vertical, 20)
-            WebImage(url: URL(string: viewModal.weatherData?.current.condition.icon ?? defaultSymbol))
+            HStack(spacing: 30) {
+                Text("""
+                Temperature
+                \(String(format: "%.2f", viewModel.weatherData?.current.tempC ?? 0.0))
+                """)
+                    .frame(width: 150)
+                    .foregroundColor(.black)
+                    .font(.bold(.headline)())
+                Text("""
+                Wind speed
+                \(String(format: "%.2f", viewModel.weatherData?.current.windKph ?? 0.0))
+                """)
+                    .frame(width: 150)
+                    .foregroundColor(.black)
+                    .font(.bold(.headline)())
+            } .padding(.vertical, 20)
+            Text(viewModel.weatherData?.current.condition.text ?? "Condition")
+                .frame(width: 100)
+                .foregroundColor(.black)
+                .font(.bold(.headline)())
+                .padding(.vertical, 20)
+            WebImage(url: URL(string: "https:" + (viewModel.weatherData?.current.condition.icon ?? "")))
                 .resizable()
                 .frame(width: 200, height: 200)
                 .foregroundColor(.black)
-            Text("Hello, world!")
             Spacer()
         }
         .padding()
         .onAppear() {
-            viewModal.fetchWeatherData(place: "tirana")
-            print(viewModal.weatherData?.current.cloud ?? "!!!")
+            viewModel.fetchWeatherData(place: "tirana")
         }
         .onChange(of: searchBtn) { newValue in
-            viewModal.fetchWeatherData(place: cityTxtLabel)
+            viewModel.fetchWeatherData(place: cityTxtLabel)
+        }
+        .alert(isPresented: $viewModel.hasError, error: viewModel.error) {
+            Button {
+                viewModel.fetchWeatherData(place: cityTxtLabel)
+            } label: {
+                Text("retry")
+            }
         }
     }
 }
--- WeatherApp/ViewModel/WeatherViewModel.swift
+++ WeatherApp/ViewModel/WeatherViewModel.swift
@@ -9,17 +9,25 @@ import SwiftUI
 
 class WeatherViewModel: ObservableObject {
     @Published var weatherData: WeatherResponse?
+    @Published var hasError = false
+    @Published var error: WeatherError?
     
     func fetchWeatherData(place: String) {
+        hasError = false
         let url = "https://api.weatherapi.com/v1/current.json?key=9c5f02690a0b45dcbcb133904241008&q=\(place)&aqi=no"
         
         guard let safeUrl = URL(string: url) else {
-            return print("invalid URL")
+            self.error = WeatherError.invalidURL
+            hasError = true
+            return
         }
         
         let weatherTask = URLSession.shared.dataTask(with: safeUrl) { [weak self] data, response, error in
-            guard let data = data, error == nil else { return }
-            print("Valid URL, going to decoding")
+            guard let data = data, error == nil else {
+                self?.error = WeatherError.custom(error: error!)
+                self?.hasError = true
+                return
+            }
             
             let decoder = JSONDecoder()
             decoder.keyDecodingStrategy = .convertFromSnakeCase
@@ -27,11 +35,35 @@ class WeatherViewModel: ObservableObject {
             do {
                 let weather = try decoder.decode(WeatherResponse.self, from: data)
                 DispatchQueue.main.async { self?.weatherData = weather }
-                print(self?.weatherData?.location.name ?? "!")
             } catch {
-                print("Failed to decode the data given")
+                DispatchQueue.main.async {
+                    self?.hasError = true
+                    self?.error = WeatherError.failedToDecode
+                }
             }
         }
         weatherTask.resume()
+    }
+}
+
+extension WeatherViewModel {
+    enum WeatherError: LocalizedError {
+        case custom(error: Error)
+        case failedToDecode
+        case invalidCity
+        case invalidURL
+        
+        var errorDescription: String? {
+            switch self {
+            case .failedToDecode:
+                return "Failed to Decode the Data"
+            case .custom(let error):
+                return error.localizedDescription
+            case .invalidCity:
+                return "Invalid City Input"
+            case .invalidURL:
+                return "Invalid URL for Request"
+            }
+        }
     }
 }
